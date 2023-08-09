fa14e2f274f5b3da29f301af477dabb46e01422e
AishaKhalfan <aishakhalfan45@gmail.com>
2023-08-10 02:22:59 +0300

    filtered logger

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction, message, separator) -> str:
+    """
+    Returns the log message obfuscated.
+    Args:
+    fields: a list of strings representing all fields to obfuscate
+    redaction: a string representing by what the field will be obfuscated
+    message: a string representing the log line
+    separator: a string representing by which character is separating
+    """
+
+    for i in fields:
+        message = re.sub(i + "=.*?" + separator, i + "=" + redaction
+                         + separator, message)
+    return message

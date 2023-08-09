b7a8b0a015b0fe5613cd7dcefa7623000dfe343b
AishaKhalfan <aishakhalfan45@gmail.com>
2023-08-10 02:26:34 +0300

    l

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,8 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction, message, separator) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """
     Returns the log message obfuscated.
     Args:

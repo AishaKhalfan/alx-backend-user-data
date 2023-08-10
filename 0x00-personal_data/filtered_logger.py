0b96d70528d1422ed47b8d1d9bd60064e21600c6
AishaKhalfan <aishakhalfan45@gmail.com>
2023-08-10 17:36:28 +0300

    secure BD

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -66,6 +66,7 @@ def get_logger() -> logging.Logger:
 
     return(logger)
 
+
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """Returns a connector to the database."""
     db_username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')

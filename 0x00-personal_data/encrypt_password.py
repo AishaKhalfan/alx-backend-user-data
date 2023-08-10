28b6d3c5c7ce11dfe585646f2942cd36ca8f6175
AishaKhalfan <aishakhalfan45@gmail.com>
2023-08-10 03:19:25 +0300

    Encrypting passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,6 +4,7 @@ Encrypting passwords
 """
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
     '''returns a salted, hashed password, which is a byte string'''
     pswd_encoded = password.encode()

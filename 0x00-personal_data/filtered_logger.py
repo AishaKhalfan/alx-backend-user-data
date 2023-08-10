006fb7989e6266a1912dfed1cd5ee30e6ce9635a
AishaKhalfan <aishakhalfan45@gmail.com>
2023-08-10 16:23:32 +0300

    logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,6 +8,31 @@ import re
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Constructor of class RedactingFormatter. """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.FIELDS = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats a log message in a human-readable format.
+        Arguments:
+            record: a log record object
+        Returns:
+            a formatted string
+        """
+        logging.basicConfig(format=self.FORMAT)
+        return(filter_datum(self.FIELDS, self.REDACTION,
+                            super().format(record), self.SEPARATOR))
+
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """
@@ -38,28 +63,3 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return(logger)
-
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        """ Constructor of class RedactingFormatter. """
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.FIELDS = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """
-        Formats a log message in a human-readable format.
-        Arguments:
-            record: a log record object
-        Returns:
-            a formatted string
-        """
-        logging.basicConfig(format=self.FORMAT)
-        return(filter_datum(self.FIELDS, self.REDACTION,
-                            super().format(record), self.SEPARATOR))

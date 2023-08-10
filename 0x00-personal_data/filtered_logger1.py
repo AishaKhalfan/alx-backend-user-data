0950e9804be4245b8ad822dc9063868e05bbe390
AishaKhalfan <aishakhalfan45@gmail.com>
2023-08-10 17:26:17 +0300

    Connect to secure database

--- 0x00-personal_data/filtered_logger1.py
+++ 0x00-personal_data/filtered_logger1.py
@@ -1,32 +1,31 @@
 #!/usr/bin/env python3
-""" 0. Regex-ing module. """
+"""
+Main file
+"""
+import re
 from typing import List
 import logging
-import re
-import os
-import mysql.connector
-
-
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
+    """ Redacting Formatter class
+        """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]):
-        """ Constructor of class RedactingFormatter. """
+    def __init__(self):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.FIELDS = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """
         Formats a log message in a human-readable format.
+
         Arguments:
             record: a log record object
+
         Returns:
             a formatted string
         """
@@ -45,39 +44,8 @@ def filter_datum(fields: List[str], redaction: str,
         message: a string representing the log line
         separator: a string representing by which character is separating
     """
+
     for i in fields:
         message = re.sub(i + "=.*?" + separator, i + "=" + redaction
                          + separator, message)
-    return(message)
-
-
-def get_logger() -> logging.Logger:
-    """takes no arguments and returns a logging.Logger object."""
-    logger = logging.getLogger("user_data")
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-
-    formatter = RedactingFormatter(PII_FIELDS)
-
-    # Create a stream handler
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
-
-    return(logger)
-
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """Returns a connector to the database."""
-    db_username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
-    db_password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
-    db_host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
-    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-
-    config = {
-        'user': db_username,
-        'password': db_password,
-        'host': db_host,
-        'database': db_name
-    }
-    connection = mysql.connector.connection.MySQLConnection(**config)
-    return(connection)
+    return message

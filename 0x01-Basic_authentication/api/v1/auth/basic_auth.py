838007b0dcf50f0beff0ae5bd2fc81a973bfd4ee
AishaKhalfan <aishakhalfan45@gmail.com>
2023-08-13 01:21:57 +0300

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Basic Auth class
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -23,3 +24,21 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+
+    def decode_base64_authorization_header(self,
+                                            base64_authorization_header: str) -> str:
+        """
+        returns the decoded value of a Base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            encoded = base64_authorization_header.encode('utf-8')
+            decoded64 = base64.b64decode(encoded)
+            return decoded64.decode('utf-8')
+        except Exception:
+            return None

f51e63578b55c0238b93dfae0ef420369360ef5a
AishaKhalfan <aishakhalfan45@gmail.com>
2023-08-14 22:29:41 +0300

    pycodestyle

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -19,9 +19,8 @@ class SessionAuth(Auth):
         creates a Session ID for a user_id
         """
         if user_id is None or not isinstance(user_id, str):
-             return None
+            return None
 
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id = {session_id: user_id}
         return session_id
-

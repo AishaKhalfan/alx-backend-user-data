f9d9bb641a61e14e3ef48936d23b74d9ef454a09
AishaKhalfan <aishakhalfan45@gmail.com>
2023-08-14 23:26:41 +0300

    User ID for Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -6,13 +6,14 @@ from api.v1.auth.auth import Auth
 from models.user import User
 from uuid import uuid4
 import uuid
+from typing import Dict
 
 
 class SessionAuth(Auth):
     """
     inherits from Auth
     """
-    user_id_by_session_id = {}
+    user_id_by_session_id: Dict = {}
 
     def create_session(self, user_id: str = None) -> str:
         """
@@ -22,14 +23,20 @@ class SessionAuth(Auth):
             return None
 
         session_id = str(uuid.uuid4())
-        self.user_id_by_session_id = {session_id: user_id}
+        # self.user_id_by_session_id = {session_id: user_id}
+        user_id_by_session_id[session_id] = user_id
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """
-         returns a User ID based on a Session ID:
+        Returns a User ID based on a Session ID:
+        Args:
+            session_id: String of the session
+        Return:
+            User ID
         """
         if session_id is None or not isinstance(session_id, str):
             return None
 
-        return self.user_id_by_session_id.get(session_id)
+        user_id: str = self.user_id_by_session_id.get(session_id)
+        return user_id
